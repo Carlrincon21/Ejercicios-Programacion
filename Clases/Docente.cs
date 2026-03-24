fa9a556520bbe4ab710febcce518bf5faa06a000
Carlos <carlrincon21@gmail.com>
2026-03-24 10:55:01 -0300

    Ejercicios Programacion

---
+++ Clases/Docente.cs
@@ -0,0 +1,12 @@
+namespace Ejemplo.Dominio;
+
+public class Docente
+{
+    private string materia;
+    public string Materia
+    {
+        get { return materia; }
+        set { materia = value; }
+    }
+    
+}
